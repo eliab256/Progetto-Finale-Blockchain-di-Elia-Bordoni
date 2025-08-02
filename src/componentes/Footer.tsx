455e691ce54c4f95fa3ed65206f14ed8ddec210a
b-elia <bordonielia96@gmail.com>
2025-08-02 18:44:05 +0200

    font added

--- src/componentes/Footer.tsx
+++ src/componentes/Footer.tsx
@@ -2,31 +2,33 @@ import '../assets/styles/Footer.css';
 
 const Footer: React.FC = () => {
     return (
-        <div className="footer-container">
-            <div className="footer-content">
-                <ul className="footer-menu">
-                    <div className="footer-column">
-                        <div className="footer-links">
-                            <div className="footer-link">
-                                <a href="https://github.com/eliab256" target="_blank" rel="noreferrer">
-                                    GitHub
-                                </a>
+        <footer>
+            <div className="footer-container">
+                <div className="footer-content">
+                    <ul className="footer-menu">
+                        <div className="footer-column">
+                            <div className="footer-links">
+                                <div className="footer-link">
+                                    <a href="https://github.com/eliab256" target="_blank" rel="noreferrer">
+                                        GitHub
+                                    </a>
+                                </div>
+                                <span className="footer-separator">·</span>
+                                <div className="footer-link">
+                                    <a href="https://t.me/Elia_EB" target="_blank" rel="noreferrer">
+                                        Support
+                                    </a>
+                                </div>
                             </div>
-                            <span className="footer-separator">·</span>
-                            <div className="footer-link">
-                                <a href="https://t.me/Elia_EB" target="_blank" rel="noreferrer">
-                                    Support
-                                </a>
-                            </div>
-                        </div>
 
-                        <div className="footer-copyright">
-                            <p>© 2025 Elia Bordoni. All rights reserved.</p>
+                            <div className="footer-copyright">
+                                <p>© 2025 Elia Bordoni. All rights reserved.</p>
+                            </div>
                         </div>
-                    </div>
-                </ul>
+                    </ul>
+                </div>
             </div>
-        </div>
+        </footer>
     );
 };
 
--- src/componentes/Gallery.tsx
+++ src/componentes/Gallery.tsx
@@ -2,6 +2,7 @@ const Gallery: React.FC = () => {
     return (
         <div>
             <h1>Gallery</h1>
+            <h1>Get your pass to the future of inner peace and mindful movement</h1>
         </div>
     );
 };
